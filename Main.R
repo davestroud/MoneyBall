ee3d9c980fca398b50d71613f857993564971fdf
davestroud <david@greymatter-analytics.com>
2018-04-04 20:12:39 -0500

    Add main.r file

---
+++ Main.R
@@ -0,0 +1,23 @@
+library(readr)
+library(tidyverse)
+
+Master <- read_csv("Baseball_Index/Master.csv")
+Batting <- read_csv("Baseball_Index/Batting.csv")
+Pitching <- read_csv("Baseball_Index/Pitching.csv")
+Fielding <- read_csv("Baseball_Index/Fielding.csv")
+Teams <- read_csv("Baseball_Index/Teams.csv")
+AllStar <- read_csv("Baseball_Index/AllstarFull.csv")
+BattingPost <- read_csv("Baseball_Index/BattingPost.csv")
+
+
+
+
+Batting_df <-filter(Batting, yearID == 2012, 2013, 2014, 2015, 2016)
+Pitching_df <- filter(Pitching, yearID == 2012, 2013, 2014, 2015, 2016)
+Fielding_df <- filter(Fielding, yearID == 2012, 2013, 2014, 2015, 2016 )
+Teams_df <- filter(Teams, yearID == 2012, 2013, 2014, 2015, 2016)
+AllStar_df <- filter(AllStar, yearID == 2012, 2013, 2014, 2015, 2016)
+BattingPost_df <- filter(BattingPost, yearID == 2012, 2013, 2014, 2015, 2016)
+
+# View its class
+dim(Batting_df)
