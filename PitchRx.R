50f9ccf7d19f2dee3bd1aa0e95b98ddab91f6803
davestroud <david@greymatter-analytics.com>
2018-04-07 18:07:09 -0500

    Have PitchRx.R file and download new dates

--- PitchRx.R
+++ PitchRx.R
@@ -1,4 +1,4 @@
 library(pitchRx)
 library(tidyverse)
 db <- src_sqlite("pitchfx.sqlite3", create = T)
-scrape(start = "2008-01-01", end = Sys.Date(), connect = db$con)
+scrape(start = "2012-01-01", end = Sys.Date(), connect = db$con)
