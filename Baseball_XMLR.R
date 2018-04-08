5fb507eec21950f9a253b3065116f5c16d1325bc
davestroud <david@greymatter-analytics.com>
2018-04-07 20:44:22 -0500

    Add tmp files and observations

--- Baseball_XMLR.R
+++ Baseball_XMLR.R
@@ -6,3 +6,18 @@ urls <- paste0(pre, post)
 obs <- XML2Obs(urls, as.equiv=TRUE, quiet=TRUE)
 table(names(obs))
 
+tmp <- re_name(obs, equiv=c("game//inning//top//atbat//pitch",
+                            "game//inning//bottom//atbat//pitch"), diff.name="side")
+tmp <- re_name(tmp, equiv=c("game//inning//top//atbat//runner",
+                            "game//inning//bottom//atbat//runner"), diff.name="side")
+tmp <- re_name(tmp, equiv=c("game//inning//top//atbat//po",
+                            "game//inning//bottom//atbat//po"), diff.name="side")
+tmp <- re_name(tmp, equiv=c("game//inning//top//atbat",
+                            "game//inning//bottom//atbat"), diff.name="side")
+obs2 <- re_name(tmp, equiv=c("game//inning//top//action",
+                             "game//inning//bottom//action"), diff.name="side")
+table(names(obs2))
+
+obs2[grep("game//inning//atbat//po", names(obs2))][1:2]
+
+obs2[grep("^game//inning$", names(obs2))][1:3]
