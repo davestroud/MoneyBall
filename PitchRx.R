d756da6e89d61faefa8d865f9641024cb3ec9a90
davestroud <david@greymatter-analytics.com>
2018-04-07 23:20:35 -0500

    Shows animation of pitches

--- PitchRx.R
+++ PitchRx.R
@@ -21,3 +21,23 @@ library(pitchRx)
 dat <- scrape(start = "2013-06-01", end = "2013-06-01")
 
 names(dat)
+
+dim(dat[["pitch"]])
+
+data(gids, package = "pitchRx")
+head(gids)
+
+MNaway13 <- gids[grep("2013_06_[0-9]{2}_minmlb*", gids)]
+dat2 <- scrape(game.ids = MNaway13)
+
+
+dat2$pitch
+
+x <- list(
+  facet_grid(pitcher_name ~ stand, labeller = label_both), 
+  theme_bw(), 
+  coord_equal()
+)
+animateFX(pitches, layer = x)
+
+
