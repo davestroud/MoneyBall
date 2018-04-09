6a3d84450cb4eb31c3f6d72ee71e3590ce04572a
davestroud <david@greymatter-analytics.com>
2018-04-08 23:19:36 -0500

    add hchart map with unemployment

--- HighCharter.R
+++ HighCharter.R
@@ -16,3 +16,15 @@ hchart(diamonds$price, color = "#B71C1C", name = "Price") %>%
   hc_title(text = "You can zoom me")
 
 
+library("forecast")
+airforecast <- forecast(auto.arima(AirPassengers), level = 95)
+hchart(airforecast)
+
+data(unemployment)
+
+hcmap("countries/us/us-all-all", data = unemployment,
+      name = "Unemployment", value = "value", joinBy = c("hc-key", "code"),
+      borderColor = "transparent") %>%
+  hc_colorAxis(dataClasses = color_classes(c(seq(0, 10, by = 2), 50))) %>%
+  hc_legend(layout = "vertical", align = "right",
+            floating = TRUE, valueDecimals = 0, valueSuffix = "%")
