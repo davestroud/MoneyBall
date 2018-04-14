d92a4470c4ca16dd0b90337b9ca4b6cce4499bc4
davestroud <david@greymatter-analytics.com>
2018-04-14 17:57:41 -0500

    Add new libraries

--- Master/Lahman_Master.R
+++ Master/Lahman_Master.R
@@ -3,13 +3,21 @@ library(ggplot2)
 library(caret)
 library(readr)
 
-Teams <- read_csv("Master/baseballdatabank-master/core/Teams.csv")
-Salaries <- read_csv("Master/baseballdatabank-master/core/Salaries.csv")
+library(glmnet)
+library(ROCR)
+library(MASS)
 
+
+Teams <- read_csv("/Users/davidstroud/Dropbox/Stats2/MoneyBall/Master/baseballdatabank-master/core/Teams.csv")
+Salaries <- read_csv("/Users/davidstroud/Dropbox/Stats2/MoneyBall/Master/baseballdatabank-master/core/Salaries.csv")
+
+# New year column, predictor, for teams that make the playoffs
 Teams$Playoff <- Teams$DivWin
 
-Teams$Playoff[Teams$WCWin == 'Y'] <- 'Y'
+# Teams$Playoff[Teams$WCWin == 'Y'] <- 'Y'
 
+# Turn Playoff into a factor variable
 Teams$Playoff <- factor(Teams$Playoff)
 
+# Teams after 1969 ~ Year that division wins were recognized.
 Teams <- filter(Teams, yearID > 1969)
