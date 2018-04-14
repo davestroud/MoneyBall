d7ad80d1bb63f796ef00ab99995e4c46b91f36a9
davestroud <david@greymatter-analytics.com>
2018-04-14 12:55:23 -0500

    loaded file from the internet

--- Lahman_Master.R
+++ Lahman_Master.R
@@ -1,3 +1,16 @@
 library(tidyverse)
 library(ggplot2)
 library(caret)
+
+
+# Baseball Zip File Url
+url <- 'http://seanlahman.com/files/database/baseballdatabank-master_2018-03-28.zip'
+# create a temp file
+temp <- tempfile()
+# fetch the file into the temp. file and download it
+download.file(url, temp)
+# extract the target file from temp. file
+teams <- read.csv(unz(temp, 'Teams.csv'))
+salaries <- read.csv(unz(temp, 'Salaries.csv'))
+
+
